98a6553e620bf18bdffb5207d44c6ea57613c3f7
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-03-02 21:52:57 -0500

    templates

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    favorite_person={
+        "name": "Maria",
+        "age": 24
+    }
+    return  render(request, 'restaurants/index.html', context=favorite_person)
