8fcb5a75fb5c926161f814c7867847610a815cdb
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-03-02 22:35:10 -0500

    templates

--- magazine/views.py
+++ magazine/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
+def articles(request):
+    article = Article.objects.all()
+    return  render(request, 'magazine/magazine.html', context={
+        "queryset":article
+    })
